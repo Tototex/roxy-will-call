ce1168a314345ca1107bd079279e33ed499747bf
Tototex <116387805+Tototex@users.noreply.github.com>
2026-03-24 10:31:11 -0700

    Update roxy-will-call.php

--- roxy-will-call.php
+++ roxy-will-call.php
@@ -2,13 +2,13 @@
 /**
  * Plugin Name: Roxy Will Call (WooCommerce)
  * Description: Will Call system for Newport Roxy. Includes showing mode, offline caching, and GitHub auto-updates.
- * Version: 0.3.4
+ * Version: 0.3.5
  * Author: Roxy AI Team
  */
 
 if (!defined('ABSPATH')) exit;
 
-define('ROXY_WC_VERSION', '0.3.4');
+define('ROXY_WC_VERSION', '0.3.5');
 define('ROXY_WC_PLUGIN_FILE', __FILE__);
 define('ROXY_WC_PLUGIN_DIR', plugin_dir_path(__FILE__));
 
